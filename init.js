d944d7d341e69ca08f8f9d4303f418677531c7de
Michal Bouska <m@bouska.eu>
2017-06-11 10:07:18 +0200

    add poly for localStorage

--- init.js
+++ init.js
@@ -8,3 +8,36 @@ if (/Edge\/\d./i.test(navigator.userAgent)) {
 if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
   sound_suff = "wav"
 }
+
+(function () {
+  function isSupported() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  if (!isSupported()) {
+    function init(undef) {
+      console.log("use local storage pol");
+      let store = {
+        setItem: function (id, val) {
+          return store[id] = String(val);
+        },
+        getItem: function (id) {
+          return store.hasOwnProperty(id) ? String(store[id]) : undef;
+        },
+        removeItem: function (id) {
+          return delete store[id];
+        },
+        clear: function () {
+          init();
+        }
+      };
+
+      window.localStorage = store;
+    }
+    init();
+  }
+}());
